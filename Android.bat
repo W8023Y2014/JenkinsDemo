660124b5dc5b42fb6f77f513b5ac11f1579ea8fa
IT-20180911OLJZ\Administrator <zhaoguanghui@shinezone.com>
2018-10-18 20:33:34 +0800

    部分的水果放到
    地方

--- Android.bat
+++ Android.bat
@@ -2,7 +2,7 @@
 
 echo Generating APK Files ...
 
-"D:\Program Files\Unity\Editor\Unity.exe" -quit -projectPath "C:\Program Files (x86)\Jenkins\workspace\JenkinsDemo"  -executeMethod Tools.APKBuild.Build -logFile build.log
+"D:\Program Files\Unity\Editor\Unity.exe" -quit -executeMethod Tools.APKBuild.Build -projectPath "C:\Program Files (x86)\Jenkins\workspace\JenkinsDemo" -logFile build.log
  
 echo The APK File Is Generated!!!
 
