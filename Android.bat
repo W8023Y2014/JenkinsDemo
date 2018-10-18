c8f8d70401803107c5f9d7fd456ace3fe90279e2
IT-20180911OLJZ\Administrator <zhaoguanghui@shinezone.com>
2018-10-18 20:23:27 +0800

    单个换个
    发

--- Android.bat
+++ Android.bat
@@ -2,7 +2,8 @@
 
 echo Generating APK Files ...
 
-%1 -projectPath %2 -batchmode -executeMethod Tools.APKBuild.Build -logFile build.log
+"D:\Program Files\Unity\Editor\Unity.exe" -quit -projectPath "C:\Program Files (x86)\Jenkins\workspace\JenkinsDemo"  -executeMethod Tools.APKBuild.Build -logFile build.log
  
 echo The APK File Is Generated!!!
-pause
+
+quit
