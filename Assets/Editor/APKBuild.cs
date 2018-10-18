e76b9150a8b343ae7018e6ad4cb6a72f58084d7b
IT-20180911OLJZ\Administrator <zhaoguanghui@shinezone.com>
2018-10-18 20:04:50 +0800

    佛挡杀佛
     多少发

--- Assets/Editor/APKBuild.cs
+++ Assets/Editor/APKBuild.cs
@@ -1,46 +1,49 @@
 ﻿using System.Collections.Generic;
 using UnityEditor;
 
-public class APKBuild : Editor
+namespace Tools
 {
-    [MenuItem("Tool/APKBuild")]
-    public static void Build()
+    public class APKBuild : Editor
     {
-        BuildTarget buildTarget = BuildTarget.Android;
-
-        // 切换到 Android 平台
-        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
-
-        //// keystore 路径, G:\keystore\one.keystore
-        //PlayerSettings.Android.keystoreName = "G:\\keystore\\one.keystore";
-
-        //// one.keystore 密码
-        //PlayerSettings.Android.keystorePass = "123456";
-
-        //// one.keystore 别名
-        //PlayerSettings.Android.keyaliasName = "bieming1";
-
-        //// 别名密码
-        //PlayerSettings.Android.keyaliasPass = "123456";
-
-        List<string> levels = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        [MenuItem("Tool/APKBuild")]
+        public static void Build()
         {
-            if (!scene.enabled)
+            BuildTarget buildTarget = BuildTarget.Android;
+
+            // 切换到 Android 平台
+            EditorUserBuildSettings.SwitchActiveBuildTarget(buildTarget);
+
+            //// keystore 路径, G:\keystore\one.keystore
+            //PlayerSettings.Android.keystoreName = "G:\\keystore\\one.keystore";
+
+            //// one.keystore 密码
+            //PlayerSettings.Android.keystorePass = "123456";
+
+            //// one.keystore 别名
+            //PlayerSettings.Android.keyaliasName = "bieming1";
+
+            //// 别名密码
+            //PlayerSettings.Android.keyaliasPass = "123456";
+
+            List<string> levels = new List<string>();
+            foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
             {
-                continue;
+                if (!scene.enabled)
+                {
+                    continue;
+                }
+
+                // 获取有效的 Scene
+                levels.Add(scene.path);
             }
 
-            // 获取有效的 Scene
-            levels.Add(scene.path);
+            // 打包出 APK 名
+            string apkName = string.Format(@"C:\{0}.apk", "Test");
+
+            // 执行打包
+            string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
+
+            AssetDatabase.Refresh();
         }
-
-        // 打包出 APK 名
-        string apkName = string.Format(@"C:\{0}.apk", "Test");
-
-        // 执行打包
-        string res = BuildPipeline.BuildPlayer(levels.ToArray(), apkName, buildTarget, BuildOptions.None);
-
-        AssetDatabase.Refresh();
     }
 }
